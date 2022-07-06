042ef8501b8903e03c6f8019e5ffb6aeee53b0f2
MohamedElmdary <engm5081@gmail.com>
2022-07-06 16:46:48 +0200

    Add OpenChat component

--- src/components/OpenChat.svelte
+++ src/components/OpenChat.svelte
@@ -2,6 +2,27 @@
 
 <script lang="ts">
   import chatStore from "../store/chatStore";
+
+  function openChat() {
+    $chatStore.open = true;
+  }
 </script>
 
-<button on:click={() => ($chatStore.open = true)}> open </button>
+<button
+  on:click={openChat}
+  style:width="60px"
+  style:height="60px"
+  style:position="fixed"
+  style:bottom="15px"
+  style:right="15px"
+  style:border-radius="50%"
+  style:background-color="#1972F5"
+  style:color="white"
+  style:border="none"
+  style:display="flex"
+  style:justify-content="center"
+  style:align-items="center"
+  style:cursor="pointer"
+>
+  <i class="fa-solid fa-message" style:font-size="28px" />
+</button>
